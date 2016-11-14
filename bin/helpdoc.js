34b35feca0dde723de8c1c6cc58f80f42f2757e5
erikEBM <howl_bm96@hotmail.com>
2016-11-13 22:35:12 -0500

    committ

--- bin/helpdoc.js
+++ bin/helpdoc.js
@@ -2,19 +2,51 @@
 const fs = require("fs");
 const path = require('path');
 const parametro = process.argv[2];
+
+// con path.resolve (__dirname) obtengo la ruta absoluta hasta mi carpeta
 const rutaabsoluta = path.resolve(__dirname) + "/..";
+
+if(parametro == null){
+    console.log("ingrese un parametro");
+    process.exit();
+    
+}
+
+
+function listarArchivos(){
+   let files = fs.readdirSync(rutaabsoluta + "/comandos/");
+   
+   files.forEach((e, i)=>{
+       console.log((i+1) + ". " + e);
+   })
+}
+
+function mostrarDoc(){
+    fs.readFile( rutaabsoluta + "/comandos/" + parametro, 'utf8', (err, fd) => {
+        if (err) {
+        console.log(err);
+        throw err; 
+        }  
+        console.log(fd);
+    });
+}
+
+switch(parametro){
+    case "ls": listarArchivos();
+               break;
+    default: mostrarDoc();
+                break;
+
+}
+
+
 
 
 
 // primer parametro significa ruta del archivo que quiero leer
 // segundo parametro sea legible para humanos
 // tercer parametro es un callback donde leo el archivo
+/*
 
-fs.readFile( rutaabsoluta + "/comandos/" + parametro, 'utf8', (err, fd) => {
-    if (err) {
-    console.log(err);
-    throw err; 
-    }  
-    console.log(fd);
-});
+*/
 
