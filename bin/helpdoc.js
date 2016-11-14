99af68ad3de7cf01bc56ac773bece72d5fff9918
erikEBM <howl_bm96@hotmail.com>
2016-11-13 21:58:26 -0500

    co

--- bin/helpdoc.js
+++ bin/helpdoc.js
@@ -1,16 +1,20 @@
 #! /usr/bin/env node
-var fs = require("fs");
-var parametro = process.argv[2];
+const fs = require("fs");
+const path = require('path');
+const parametro = process.argv[2];
+const rutaabsoluta = path.resolve(__dirname) + "/..";
 
 
 
 // primer parametro significa ruta del archivo que quiero leer
 // segundo parametro sea legible para humanos
 // tercer parametro es un callback donde leo el archivo
-fs.readFile('../comandos/' + parametro, 'utf8', (err, fd) => {
+
+fs.readFile( rutaabsoluta + "/comandos/" + parametro, 'utf8', (err, fd) => {
     if (err) {
     console.log(err);
     throw err; 
     }  
     console.log(fd);
 });
+
